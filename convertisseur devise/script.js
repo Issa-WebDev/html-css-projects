29fbfeee735c8fabae435f38ce8af963974c3e44
KissCoder <167043658+Issa-WebDev@users.noreply.github.com>
2025-03-03 06:01:32 +0100

    Update script.js

--- convertisseur devise/script.js
+++ convertisseur devise/script.js
@@ -1,7 +1,9 @@
 const form = document.getElementById("form");
 const euroInput = document.getElementById("euro");
 const cfaInput = document.getElementById("cfa");
+const allBtn = document.querySelectorAll(".control div button");
 const tauxFixe = 655.957;
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -10,19 +12,21 @@ form.addEventListener("submit", (e) => {
 
 // CONVERTIR LA VALEUR
 const convert = () => {
-  const euro = Number(euroInput.value);
+  const euro = parseFloat(euroInput.value);
   if (!euroInput.value.trim()) {
     alert("Entrer votre montant");
     return;
   }
-  const result = euro * tauxFixe;
+  const result = (euro * tauxFixe).toFixed(2);
   cfaInput.value = `${result} XOF`;
 };
 
 // AJOUTE UNE VALEUR
-const addValue = (value) => {
-  euroInput.value += value;
-};
+allBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    euroInput.value += button.textContent;
+  });
+});
 
 // ASUPPRIME UNE VALEUR
 const removeOneByOne = () => {
